e18f66c1bf8a8cf26c0e250c74abed511e7a052b
Renan Bispo <renanbispouniversity@gmail.com>
2024-08-09 09:39:02 -0300

    criando classe de itens do carrinho de compras

---
+++ src/main/java/list/BasicOperations/ShoppingCart/item.java
@@ -0,0 +1,25 @@
+package list.BasicOperations.ShoppingCart;
+
+public class item {
+    private String name;
+    private double price;
+    private int amount;
+
+    public item(String name, double price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
