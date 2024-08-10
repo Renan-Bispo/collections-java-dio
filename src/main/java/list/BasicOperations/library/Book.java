cfe9c790328d106246948cca8e910a4db7965a31
Renan Bispo <renanbispouniversity@gmail.com>
2024-08-10 08:27:31 -0300

    init: iniciando atividade "biblbioteca" criando a classe livro

---
+++ src/main/java/list/BasicOperations/library/Book.java
@@ -0,0 +1,31 @@
+package list.BasicOperations.library;
+
+public class Book {
+    private String title;
+    private String author;
+    private int yearOfRelease;
+
+    public Book(String title, String author, int yearOfRelease) {
+        this.title = title;
+        this.author = author;
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "title = " + title + ", author = " + author + ", yearOfRelease= " + yearOfRelease + '}';
+    }
+}
+
